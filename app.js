f588146e3d6a25390c39998fb8c90e95095ac80e
Mathias Truschel <mtruschel@gmail.com>
2023-06-23 20:27:10 +0200

    exchange the Axios-Test for a PINValidation function

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-import { AxiosInstance } from 'axios';
-console.log('this is a test.');
+function validatePIN(pin) {
+    const validNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    numbersIncluded = false;
+    for(let p of pin.toString()) {
+        if(validNumbers.includes(p)) { numbersIncluded = true; }
+        else { numbersIncluded = false; break; }
+    }
+    
+    if ((pin.toString().length == 4 || pin.toString().length == 6) && numbersIncluded) {
+        return true;
+    }
+    else return false;
+    
+}
 
-const tempo = new AxiosInstance;
-
-tempo.console;
+console.log(validatePIN("i234"));
